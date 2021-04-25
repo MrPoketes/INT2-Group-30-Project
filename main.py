16726c781c5475fdd9a27004cc9224608b97e7f3
MrPoketes <az778@york.ac.uk>
2021-04-25 16:58:14 +0300

    armintas: Standardized images

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib as plt
+from keras.utils.np_utils import to_categorical
 
-# Download dataset
+# Download / load dataset
 
 cifor10_df = tf.keras.datasets.cifar10
 
 (train_images, train_labels), (test_images, test_labels) = cifor10_df.load_data()
 
-print(train_images)
+# Transform labels to 1 hot encodings
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+# Convert images to have a range between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
