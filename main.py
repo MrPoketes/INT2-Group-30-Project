26f277561065d3d885074f3b93f0fe9c4f626508
mrk531 <72560774+mrk531@users.noreply.github.com>
2021-04-27 23:51:08 +0100

    Tried imporoving the model

--- main.py
+++ main.py
@@ -18,27 +18,31 @@ train_labels = to_categorical(train_labels)
 test_labels = to_categorical(test_labels)
 """
 
+
 # Convert images to have a range between 0 and 1
 train_images, test_images = train_images / 255.0, test_images / 255.0
 
 
-
+# Model
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
 
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Flatten())
 model.add(layers.Dense(10))
 
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-history = model.fit(train_images, train_labels, epochs=5, 
+history = model.fit(train_images, train_labels, epochs=25, 
                     validation_data=(test_images, test_labels))
 
 plt.plot(history.history['accuracy'], label='accuracy')
@@ -50,3 +54,4 @@ plt.legend(loc='lower right')
 plt.show()
 
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+print(test_loss, test_acc)
