e2e7a5f94ddce8607da22f84f70c8d06cd223d58
mrk531 <72560774+mrk531@users.noreply.github.com>
2021-04-28 15:05:43 +0100

    newer model not much better

--- main.py
+++ main.py
@@ -35,13 +35,10 @@ model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Flatten())
-model.add(layers.Dense(32, activation='relu'))
-model.add(layers.Flatten())
 model.add(layers.Dense(10))
 
 model.compile(optimizer='adam',
