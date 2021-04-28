333a6edb00f026df62e157af909a2ee36b346721
mrk531 <72560774+mrk531@users.noreply.github.com>
2021-04-28 14:20:49 +0100

    Added data augmentation

--- main.py
+++ main.py
@@ -25,6 +25,12 @@ train_images, test_images = train_images / 255.0, test_images / 255.0
 
 # Model
 model = models.Sequential()
+
+# Augmentation to increase val_acurracy
+model.add(layers.experimental.preprocessing.RandomFlip("horizontal", input_shape=(32, 32, 3)))
+model.add(layers.experimental.preprocessing.RandomRotation(0.1))
+model.add(layers.experimental.preprocessing.RandomZoom(0.1))
+
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
